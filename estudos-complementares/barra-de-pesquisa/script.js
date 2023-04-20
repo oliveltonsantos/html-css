66ed8403e9f3f2d2f618943ddce366ed909296b8
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-04-20 12:11:47 -0300

    Search do zero

--- estudos-complementares/barra-de-pesquisa/script.js
+++ estudos-complementares/barra-de-pesquisa/script.js
@@ -7,14 +7,14 @@ icon.onclick = function() {
     search.classList.toggle('ativo')
 }
 
-clear.onclick = function() {
+clear.onclick = function(){
     search.classList.remove('ativo');
-
     input.value = '';
-}
-
-
-
-
-
-
+  }
+  
+input.addEventListener('keypress', function(e) {
+    if(e.key === "Enter"){
+        const url = 'https://www.google.com/search?q=' + input.value;
+        window.open(url, '_self');
+    }
+})
