8257ec5d26935edc4d9e2ceb10245912c18566c3
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-04-21 12:05:48 -0300

    SLIDES de Imagens

---
+++ estudos-complementares/slides-imagens/script.js
@@ -0,0 +1,13 @@
+let contador = 1;
+
+setInterval(function (){
+
+    document.getElementById('radio-' + contador).checked = true;
+    contador ++;
+
+    if(contador > 4){
+        contador = 1
+    }
+
+}, 4000)
+
