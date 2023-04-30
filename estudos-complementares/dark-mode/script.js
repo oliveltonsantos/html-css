9cc9c6fd188e9933d96c8cbad17c1d028fb546e1
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-04-30 12:23:30 -0300

    update

--- estudos-complementares/dark-mode/script.js
+++
@@ -1,7 +0,0 @@
-const header = document.querySelector('.header');
-const icon = document.querySelector('.icon');
-
-icon.addEventListener('click', () => {
-    header.classList.toggle('dark');
-})
-
--- estudos-complementares/munu-responsivo/script.js
+++
@@ -1,8 +0,0 @@
-const hamburguer = document.querySelector('.hamburguer');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburguer.addEventListener('click', () => {
-    hamburguer.classList.toggle('active');
-    navMenu.classList.toggle('active');
-
-})
